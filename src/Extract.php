b4f67f5963b1066b5fb723600231ea2218b522f6
Sir McHorse <mchorselessrider@gmail.com>
2016-08-13 21:06:50 +0100

    Create Extract.php

---
+++ src/Extract.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace crystal\edge;
+
+/**
+ * Web page extractor interface
+ * 
+ * This interface is responsible for implementing extraction of 
+ * web pages from some source like data base or file system.
+ */
+interface Extract
+{
+  /**
+   * Extract web pages from some (DB, cache, FS, array) source 
+   * 
+   * @return array
+   */
+  public function extract();
+}
