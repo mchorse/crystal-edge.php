8d797a870d034929af413d20dc0e71b20f263317
Sir McHorse <mchorselessrider@gmail.com>
2016-08-13 21:05:19 +0100

    Create Site.php

---
+++ src/Site.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Site class
+ *
+ * This class is responsible for storing extracted web pages and
+ * process them via given filters.
+ */
+class Site
+{
+  /**
+   * @var array $pages Hash map of web pages (path => data)
+   */
+  protected $pages = [];
+  
+  /**
+   * @var array $filters Callable filters which responsible for modifying data
+   */
+  public $filters = [];
+  
+  /**
+   * Initiate with extraction method
+   */
+  public function __construct(Extraction $extract)
+  {
+    $this->pages = $extract->extract();
+  }
+  
+  /** 
+   * Process pages via filters and return the content
+   * 
+   * @return array
+   */
+  public function process()
+  {
+    $pages = $this->pages;
+
+    foreach ($this->filters as  $filter)
+    {
+      $pages = $filter($pages);
+    }
+    
+    return $pages ?: [];
+  }
+}
