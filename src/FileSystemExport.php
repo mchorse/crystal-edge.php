6b63bf795662c3165dd6876a83cc340f39a90382
Sir McHorse <mchorselessrider@gmail.com>
2016-08-14 08:37:36 +0100

    File system implementation of export

---
+++ src/FileSystemExport.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace crystal\edge;
+
+/**
+ * File system export implementation
+ *
+ * This class is responsible for exporting given instance of 
+ * Site to file system folder.
+ */
+class FileSystemExport implements Export
+{
+  /**
+   * @var \crystal\edge\Site $site An instance of Site class
+   */
+  private $site;
+  
+  /**
+   * Folder to which an instance of Site going to be exported
+   */
+  private $path = '';
+  
+  /**
+   * Initiate this class with aite and build path
+   */
+  public function __construct(Site $site, $path)
+  {
+    $this->site = $site;
+    $this->path = $path;
+  }
+  
+  /**
+   * Export a site to build folder
+   */
+  public function export()
+  {
+    foreach ($this->site->process() as $path => $content)
+    {
+      $path = "{$this->path}/$path";
+      $folder = substr($path, strpos($path, '/'));
+      
+      if (!is_dir($folder)) mkdir($folder, 0777, true);
+      
+      file_put_contents($path, $content['output']);
+    }
+    
+    return true;
+  }
+}
