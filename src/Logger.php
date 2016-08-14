6ecca28876bf0e660d47b1944a45f6698841a6a4
Sir McHorse <mchorselessrider@gmail.com>
2016-08-14 10:32:34 +0100

    Create Logger interface

---
+++ src/Logger.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace crystal\edge;
+
+/**
+ * Logger interface
+ *
+ * Responsible for logging, duh.
+ */
+interface Logger
+{
+  /* Severity log levels */
+  const INFO = 0;
+  const WARN = 1;
+  const ERROR = 2;
+  
+  /**
+   * Logs the message
+   *
+   * @param int $level The severity level of log
+   * @param string $string Format string
+   * @param array $args Arguments to be used in formatting the $string argument
+   */
+  public function log($level, $string, array $args);
+}
